7c2698c85493a234da3d633dffaaa079737fd9e1
Phaedrus3 <131749747+Phaedrus3@users.noreply.github.com>
2023-04-30 18:09:16 -0500

    Add project files.

---
+++ Properties/Implementation.cs
@@ -0,0 +1,14 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace ShelterTemperatureTweaks
+{
+    public class Implementation : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Debug.Log($"[{Info.Name}] version {Info.Version} loaded!");
+            Settings.OnLoad();
+        }
+    }
+}
